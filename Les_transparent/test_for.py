81a60e8b2ab46d7924d1a4e779be22ca1cf30371
Malek <toumiab@ensta-bretagne.fr>
2015-09-29 23:15:51 +0200

    ajouts des boucles et fin de cours 2

---
+++ Les_transparent/test_for.py
@@ -0,0 +1,14 @@
+# affiche 'hello World" : commentaire sur une ligne
+""" Ceci est mon premier programme
+avec un commentaire fait sur plusieurs lignes """
+print("Hello World!")
+
+##
+##
+##
+##
+##s1=['septembre','octobre']
+##s='c\'est  une chaîne de caractère'
+##s={'a':10,'b':'deux'}
+##for ind, val in enumerate(s1):
+##    print(ind, val, end = ' ')
